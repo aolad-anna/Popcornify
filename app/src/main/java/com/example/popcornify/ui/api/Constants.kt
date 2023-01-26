012bc757a22eb88a7977c0576da3b1528ec1e0bf
Aolad Hossain Anna <aolad.hossain@nexdecade.com>
2023-01-26 10:47:48 +0600

    new changes

--- app/src/main/java/com/example/popcornify/ui/api/Constants.kt
+++ app/src/main/java/com/example/popcornify/ui/api/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.popcornify.ui.api
 
-const val BASE_URL = " "
+const val BASE_URL = "https://covid19.aoladanna.info/"
 
 const val MOVIE_POINT = "movies.php"
 
--- app/src/main/java/com/example/popcornify/ui/home/landingpage/carousel/CarouselFragment.kt
+++ app/src/main/java/com/example/popcornify/ui/home/landingpage/carousel/CarouselFragment.kt
@@ -2,17 +2,17 @@ package com.example.popcornify.ui.home.landingpage.carousel
 
 import Carousel
 import android.annotation.SuppressLint
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.load.engine.Resource
 import com.example.popcornify.R
 import com.example.popcornify.databinding.FragmentCarouselBinding
 import com.example.popcornify.ui.adapter.RecyclerAdapterCarousel
@@ -26,12 +26,10 @@ import retrofit2.Response
 class CarouselFragment : Fragment() {
 
     private lateinit var carouselViewModel: CarouselViewModel
-
-    lateinit var carouselRecyclerview: RecyclerView
-    lateinit var carouselRecyclerAdapter: RecyclerAdapterCarousel
+    private lateinit var carouselRecyclerview: RecyclerView
+    private lateinit var carouselRecyclerAdapter: RecyclerAdapterCarousel
 
     private var _binding: FragmentCarouselBinding? = null
-
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -47,49 +45,68 @@ class CarouselFragment : Fragment() {
         val toolbar = activity?.findViewById<TextView>(R.id.message_tv)
         toolbar?.text = "Popcornify"
 
-        val carouselRecyclerview = binding.root.findViewById<CarouselRecyclerview>(R.id.recycler)
+        carouselRecyclerview = binding.root.findViewById<CarouselRecyclerview>(R.id.recycler)
         carouselRecyclerAdapter = RecyclerAdapterCarousel(requireContext())
         carouselRecyclerview.adapter = carouselRecyclerAdapter
-        carouselRecyclerview.set3DItem(true)
-        carouselRecyclerview.setAlpha(true)
-        val carouselLayoutManager = carouselRecyclerview.getCarouselLayoutManager()
-        val currentlyCenterPosition = carouselRecyclerview.getSelectedPosition()
+        (carouselRecyclerview as CarouselRecyclerview?)?.set3DItem(true)
+        (carouselRecyclerview as CarouselRecyclerview?)?.setAlpha(true)
+        val carouselLayoutManager = (carouselRecyclerview as CarouselRecyclerview?)?.getCarouselLayoutManager()
+        val currentlyCenterPosition = (carouselRecyclerview as CarouselRecyclerview?)?.getSelectedPosition()
 
         val apiService = ApiClient.client!!.create(ApiInterface::class.java)
         val call: Call<List<Carousel>> = apiService.getCarousel()
         call.enqueue(object : Callback<List<Carousel>> {
-            @SuppressLint("SetTextI18n")
             override fun onResponse(call: Call<List<Carousel>>, response: Response<List<Carousel>>) {
-
-                if (response != null) {
-                    response.body()?.let { carouselRecyclerAdapter.setCarouselListItems(it)}
-                    val controll = activity?.findViewById<View>(R.id.carouselCardPlace)
-                    controll?.visibility=View.GONE
-                    _binding!!.carouselCard.visibility=View.VISIBLE
-                    var x = 0
-                    if (x == 0){
-                        Handler().postDelayed({carouselLayoutManager.scrollToPosition(x)}, 3000)
-                        x++
-                        Log.i("LOG","Print: $x")
+                try {
+                    if (response.isSuccessful) {
+                        response.body()?.let { carouselRecyclerAdapter.setCarouselListItems(it)}
+                        val placeHolder = activity?.findViewById<View>(R.id.carouselCardPlace)
+                        placeHolder?.visibility=View.GONE
+                        _binding!!.carouselCard.visibility=View.VISIBLE
+                        autoScrollCarousel()
                     }
+                    else
+                        Toast.makeText(context, "Something went to wrong!!", Toast.LENGTH_LONG).show()
                 }
-                else {
-                    Toast.makeText(activity, "response Failed", Toast.LENGTH_SHORT).show();
+                catch (e: Exception) {
+                    throw e
                 }
             }
 
             override fun onFailure(call: Call<List<Carousel>>, t: Throwable) {
                 if (call.isCanceled) {
-                    Toast.makeText(context, "Response is Canceled", Toast.LENGTH_LONG).show();
-
-                }else {
-                    Toast.makeText(activity, "No internet connection!", Toast.LENGTH_SHORT).show();
+                    t.printStackTrace()
+                }
+                else {
+                    t.printStackTrace()
                 }
             }
         })
-
         return root
     }
+
+    private fun autoScrollCarousel() {
+        val speedScroll = 3500
+        val handler = Handler()
+        val runnable: Runnable = object : Runnable {
+            var count = 0
+            var flag = true
+            override fun run() {
+                if (count < carouselRecyclerAdapter.itemCount) {
+                    if (count == carouselRecyclerAdapter.itemCount - 1) {
+                        flag = false
+                    } else if (count == 0) {
+                        flag = true
+                    }
+                    if (flag) count++ else count--
+                    carouselRecyclerview.smoothScrollToPosition(count)
+                    handler.postDelayed(this, speedScroll.toLong())
+                }
+            }
+        }
+        handler.postDelayed(runnable, speedScroll.toLong())
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
